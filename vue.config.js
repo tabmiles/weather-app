62b4af67149cf322fa3dd30688acdb4da8653d21
tabmiles <tabithamiles0@gmail.com>
2025-03-27 12:58:07 -0700

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/weather-app/' : '/',
-})
+}
